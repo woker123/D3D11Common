bb1cc4d68d38780170f70bba018fcfd53ff7d5ef
woker123 <1092666050@qq.com>
2020-08-10 21:55:51 +0800

    fix unnecessary member variable

--- Source/D3D11Shader.h
+++ Source/D3D11Shader.h
@@ -20,7 +20,6 @@ namespace D3DShader
 	public:
 		ShaderClass(const wchar_t* fileName, bool debug)
 		{
-			Func = func;
 			bool init_shader =
 				initVertexShader(fileName, debug);
 			assert(init_shader);
@@ -50,7 +49,7 @@ namespace D3DShader
 			D3DCompileFromFile(fileName, NULL, NULL, "main", profiles[profileIndex], flag, 0, &m_shaderBlob, NULL);
 			if (FAILED(result)) return false;
 			ID3D11Device* device = D3D11System::getInstance()->getDevice().Get();
-			result = (device->*Func)(
+			result = (device->*func)(
 				m_shaderBlob->GetBufferPointer(), m_shaderBlob->GetBufferSize(), NULL, &m_shader);
 			return SUCCEEDED(result);
 		}
@@ -58,7 +57,6 @@ namespace D3DShader
 	private:
 		ComPtr<D3DShaderType> m_shader;
 		ComPtr<ID3D10Blob> m_shaderBlob;
-		FuncType Func;
 	};
 
 	typedef ShaderClass<ID3D11VertexShader, decltype(&ID3D11Device::CreateVertexShader), &ID3D11Device::CreateVertexShader, 0> VertexShader;
